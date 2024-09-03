32c58870fbce5b37f15b0ca2682fa8f6de829b34
Hồ Sơn Hào <148406980+haodz2k4@users.noreply.github.com>
2024-09-03 15:30:56 +0700

    connected to mysql

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"ArticleVista/config"
+	"fmt"
+	"log"
+)
+
+func main() {
+	db, err := config.NewConnection()
+	if err != nil {
+		log.Fatalln("Cannot connect to database: ", err.Error())
+	}
+	fmt.Println(db)
+}
