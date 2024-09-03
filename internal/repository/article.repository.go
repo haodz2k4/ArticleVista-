1bbca807177a5cee30a7d28f4b4f12c6c04d42e7
Hồ Sơn Hào <148406980+haodz2k4@users.noreply.github.com>
2024-09-03 16:28:03 +0700

    CRUD articles

--- internal/repository/article.repository.go
+++ internal/repository/article.repository.go
@@ -1,11 +1,53 @@
 package repository
 
-import "ArticleVista/internal/model"
+import (
+	"ArticleVista/internal/model"
+	"gorm.io/gorm"
+)
 
-type ArticleResponsitory interface {
+type ArticleRepository interface {
 	CreateArticle(article *model.Articles) error
 	GetArticleById(id int) (*model.Articles, error)
 	GetAllArticles() ([]model.Articles, error)
 	UpdateArticle(article *model.Articles) error
 	DeleteArticle(id int) error
+}
+
+type articleRepository struct {
+	db *gorm.DB
+}
+
+func NewArticleRepository(db *gorm.DB) ArticleRepository {
+	return &articleRepository{db: db}
+}
+
+func (a *articleRepository) CreateArticle(article *model.Articles) error {
+	return a.db.Create(article).Error
+}
+
+func (a *articleRepository) GetArticleById(id int) (*model.Articles, error) {
+	var article *model.Articles
+	if err := a.db.Take(&article, id).Error; err != nil {
+		return nil, err
+	}
+	return article, nil
+}
+
+func (a *articleRepository) GetAllArticles() ([]model.Articles, error) {
+	var users []model.Articles
+
+	if err := a.db.Find(&users).Error; err != nil {
+		return nil, err
+	}
+	return users, nil
+}
+
+func (a *articleRepository) UpdateArticle(article *model.Articles) error {
+	return a.db.Save(&article).Error
+
+}
+
+func (a *articleRepository) DeleteArticle(id int) error {
+	var article model.Articles
+	return a.db.Table(article.TableName()).Delete(nil, id).Error
 }
