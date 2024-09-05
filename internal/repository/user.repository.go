ee1550a3b6079a1bf3494e500e5fef6e33dcb516
Hồ Sơn Hào <148406980+haodz2k4@users.noreply.github.com>
2024-09-05 13:03:00 +0700

    build user repository

--- internal/repository/user.repository.go
+++ internal/repository/user.repository.go
@@ -6,27 +6,34 @@ import (
 )
 
 type UserRepository interface {
-	GetAllUserByQuery() ([]model.Users, error)
-	GetUserById(id int) (*model.Users, error)
-	UpdateUserById(id int, user *model.Users) error
-	DeleteUserById(id int) error
 	CreateUser(user *model.Users) error
+	UpdateUserById(id int, user model.Users) error
+	GetUserById(id int) (*model.Users, error)
+	GetUsers() ([]model.Users, error)
+	DeleteUserById(id int) error
 }
 
 type userRepository struct {
 	db *gorm.DB
 }
 
-func NewUserRepository(db *gorm.DB) *userRepository {
-	return &userRepository{db}
+func NewUserRepository(db *gorm.DB) UserRepository {
+	return &userRepository{db: db}
 }
 
-func (u *userRepository) GetAllUserByQuery() ([]model.Users, error) {
-	var users []model.Users
-	if err := u.db.Find(&users).Error; err != nil {
-		return nil, err
+func (u *userRepository) CreateUser(user *model.Users) error {
+
+	if err := u.db.Create(&user).Error; err != nil {
+		return err
 	}
-	return users, nil
+	return nil
+}
+
+func (u *userRepository) UpdateUserById(id int, user model.Users) error {
+	if err := u.db.Updates(user).Error; err != nil {
+		return err
+	}
+	return nil
 }
 
 func (u *userRepository) GetUserById(id int) (*model.Users, error) {
@@ -35,27 +42,21 @@ func (u *userRepository) GetUserById(id int) (*model.Users, error) {
 		return nil, err
 	}
 	return user, nil
+
 }
 
-func (u *userRepository) UpdateUserById(id int, user *model.Users) error {
-
-	if err := u.db.Where("id = ?", id).Updates(&user).Error; err != nil {
-		return err
+func (u *userRepository) GetUsers() ([]model.Users, error) {
+	var users []model.Users
+	if err := u.db.Find(&users).Error; err != nil {
+		return nil, err
 	}
-	return nil
+	return users, nil
+
 }
 
 func (u *userRepository) DeleteUserById(id int) error {
 	var user *model.Users
-	if err := u.db.Delete(&user, id).Error; err != nil {
-		return err
-	}
-	return nil
-}
-
-func (u *userRepository) CreateUser(user *model.Users) error {
-
-	if err := u.db.Create(&user).Error; err != nil {
+	if err := u.db.Delete(user, id).Error; err != nil {
 		return err
 	}
 	return nil
